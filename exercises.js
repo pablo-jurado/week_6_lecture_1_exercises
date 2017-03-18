6b56af5908534a927b48ba3072fe584418d58a65
Pablo Jurado <pjurado85@gmail.com>
2017-03-18 00:16:44 -0500

    add answers and exercises files

---
+++ exercises.js
@@ -0,0 +1,17 @@
+// In your own words, explain how object literals are used to store related
+// information and why that is useful.
+// Write a short program in which you create two objects whose properties
+// describe fictional characters. Output both objects to the console.
+// Write a small program that does the following:
+// Create an object using object literal syntax with at least five properties.
+// For example, your object might represent a person and include properties for
+// name, age, number of children, etc.
+// Output the object directly to the console.
+// Write a string that describes the object using the properties you created.
+// For example, "Dan is a 35 year old father of 2 kids." Be sure to access the
+// properties using the dot property accessor.
+// Change the value of one of the properties using dot notation.
+// Add a new property to the object using dot notation.
+// Output the object directly to the console.
+// Write a new string that describes the object using all of the object's
+// properties. Be sure to include the property you just created.
